2738871186289587f48964b5803ba434784eebd1
Rob Nielsen <rob@refactor.com.au>
2009-02-26 21:53:23 +1000

    Removed signout links at top of login page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,6 @@
 class UserController < ApplicationController
-
+  before_filter :authenticate
+  
   def index
     @duser = User.fetch(params[:username])
     @tweets = @duser.public_tweets.find(:all,:include => :user,:limit => 20  )
