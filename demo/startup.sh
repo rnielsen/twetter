8ab98c88177bbe9879b4a18e75ca12b0ea5f8ac9
Rob Nielsen <rob@refactor.com.au>
2009-02-26 19:37:44 +1000

    Added demo code for running twetter on a ad-hoc wifi network

---
+++ demo/startup.sh
@@ -0,0 +1,12 @@
+killall bootpd
+killall lighttpd
+/sbin/ifconfig en1 10.0.2.1 netmask 255.255.255.0 broadcast 10.0.2.255
+cp demo/bootpd.plist /etc
+cp /etc/hosts /etc/hosts.tworig
+echo "10.0.2.1 twitter.com twetter.com google.com facebook.com" >> /etc/hosts
+/usr/libexec/bootpd -D
+lighttpd -f config/lighttpd.conf
+python demo/DNSQuery.py
+killall bootpd
+killall lighttpd
+mv /etc/hosts.tworig /etc/hosts
