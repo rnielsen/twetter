d0e7f5cb68a0a80b9d18d9c250f5be7461b64cd2
Rob Nielsen <rob@refactor.com.au>
2009-02-26 20:08:45 +1000

    Fixed lighttpd configuration after moving

--- demo/startup.sh
+++ demo/startup.sh
@@ -5,7 +5,7 @@ cp demo/bootpd.plist /etc
 cp /etc/hosts /etc/hosts.tworig
 echo "10.0.2.1 twitter.com twetter.com google.com facebook.com" >> /etc/hosts
 /usr/libexec/bootpd -D
-lighttpd -f config/lighttpd.conf
+lighttpd -f demo/lighttpd.conf
 python demo/DNSQuery.py
 killall bootpd
 killall lighttpd
